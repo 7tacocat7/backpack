7c38eabffae8cca8cdbe5b11c84c7ed363bb9673
Carson Canales and Collin Person <carson@>
2017-08-10 15:42:18 -0700

    fix issues in .hbs files to display content when app run - replaced content with respective fields (map,things,wallet)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -33,7 +33,7 @@ public class App {
             String content = request.queryParams("content");
             Backpack newBackpack = new Backpack(mapBoolean,content,wallet);
             model.put("post",newBackpack);
-            return new ModelAndView(model, "newpost-form.hbs");
+            return new ModelAndView(model, "success.hbs");
         }, new HandlebarsTemplateEngine());
 
         get("/posts/:id", (req, res) -> {
@@ -41,7 +41,13 @@ public class App {
             int idOfPostToFind = Integer.parseInt(req.params("id")); //pull id - must match route segment
             Backpack foundBackpack = Backpack.findById(idOfPostToFind); //use it to find backpack
             model.put("post", foundBackpack); //add it to model for template to display
-            return new ModelAndView(model, "post-detail.hbs"); //individual post page.
+            return new ModelAndView(model, "post-form.hbs"); //individual post page.
+        }, new HandlebarsTemplateEngine());
+        get("/posts", (req, res) -> {
+            Map<String , Object> model = new HashMap<>();
+            ArrayList<Backpack> backpacks = Backpack.getAll();
+            model.put("posts", backpacks);
+            return new ModelAndView(model, "allPosts.hbs");
         }, new HandlebarsTemplateEngine());
     }
 
